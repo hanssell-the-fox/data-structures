6be608c80770b3b6b96f0b33b125b5e3d95f62d0
Wederson Calixto <wederson.programador@gmail.com>
2023-02-26 22:56:29 -0300

    add problem n-1

---
+++ problems/problem-1.ts
@@ -0,0 +1,15 @@
+// Given 2 arrays, create a function that returns let's the user know (true/false),
+// whether these two arrays contain any common items
+//
+// For example:
+//  const array1 = ['a', 'b', 'c', 'x'];
+//  const array2 = ['z', 'y', 'i'];
+//  should return false
+// --
+//  const array1 = ['a', 'b', 'c', 'x'];
+//  const array2 = ['z', 'y', 'x'];
+//  should return true
+
+// Observations:
+// Has 2 parameters - Always is arrays - Has no size limit
+// Returns TRUE/FALSE
