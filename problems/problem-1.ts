7af64cf450c5cd802703ebbc5c72de5811afb1ee
Wederson Calixto <wederson.programador@gmail.com>
2023-02-26 23:06:53 -0300

    first solution for problem-1

--- problems/problem-1.ts
+++ problems/problem-1.ts
@@ -1,3 +1,5 @@
+// deno-lint-ignore-file no-unused-vars
+
 // Given 2 arrays, create a function that returns let's the user know (true/false),
 // whether these two arrays contain any common items
 //
@@ -13,3 +15,25 @@
 // Observations:
 // Has 2 parameters - Always is arrays - Has no size limit
 // Returns TRUE/FALSE
+
+// TESTS
+{
+  const array1 = ["a", "b", "c", "x"];
+  const array2 = ["z", "y", "x"];
+  const array3 = ["z", "y", "i"];
+
+  console.log(containsCommonItem(array1, array2)); // should return true
+  console.log(containsCommonItem(array1, array3)); // should return false
+}
+
+// Brute-force approach for this solution
+// O(a*b)
+function containsCommonItem(arr1: unknown[], arr2: unknown[]): boolean {
+  for (const item of arr1) {
+    for (const match of arr2) {
+      if (item === match) return true;
+    }
+  }
+
+  return false;
+}
