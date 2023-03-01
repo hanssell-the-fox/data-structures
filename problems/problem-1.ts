56d7bd0a8ffa82901a79076dd987666d5075010e
Wederson Calixto <wederson.programador@gmail.com>
2023-02-28 23:13:31 -0300

    refactor: problem-1 solutions

--- problems/problem-1.ts
+++ problems/problem-1.ts
@@ -10,39 +10,52 @@
 //  const array2 = ['z', 'y', 'x'];
 //  should return true
 
+// --
 // Observations:
 // Has 2 parameters - Always is arrays - Has no size limit
 // Returns TRUE/FALSE
+// --
 
-import { KeyMap } from "./types.ts";
-
-// Brute-force approach (Not efficient O(a*b)
+// O(a*b) Runtime - Brute-force approach (Not efficient)
 export function containsCommonItem(
-  items: unknown[],
-  otherItems: unknown[],
+  firstArray: unknown[],
+  secondArray: unknown[],
 ): boolean {
-  for (const item of items) {
-    for (const match of otherItems) {
-      if (item === match) return true;
+  for (const item of firstArray) {
+    for (const otherItem of secondArray) {
+      if (item === otherItem) return true;
     }
   }
 
   return false;
 }
 
-// Efficient approach for this solution
-// O(a+b)
-export function containsCommonItem2(arr1: unknown[], arr2: unknown[]): boolean {
-  const mappedArray = new KeyMap().from(arr1);
-  return mapContainsItem(mappedArray, arr2);
+// O(a+b) Runtime - Efficient approach
+export function containsCommonItem2(
+  firstArray: unknown[],
+  secondArray: unknown[],
+): boolean {
+  const mappedItems = createMapFrom(firstArray);
+  return hasEqualItem(mappedItems, secondArray);
 }
 
-function mapContainsItem(map: KeyMap, array: unknown[]): boolean {
-  return array.some((item) => map.contains(item as string));
+function createMapFrom(array: unknown[]): Map<string, boolean> {
+  const mapOfItems = new Map<string, boolean>();
+  array.forEach((item) => mapOfItems.set(`${item}`, true));
+  return mapOfItems;
 }
 
-// Languages efficient approach
-//  O(a+b)
-export function containsCommonItem3(arr1: unknown[], arr2: unknown[]): boolean {
-  return arr1.some((item) => arr2.includes(item));
+function hasEqualItem(
+  map: Map<string, boolean>,
+  array: unknown[],
+): boolean {
+  return array.some((item) => map.has(`${item}`));
+}
+
+// O(a+b) Runtime - Languages efficient approach
+export function containsCommonItem3(
+  firstArray: unknown[],
+  secondArray: unknown[],
+): boolean {
+  return firstArray.some((item) => secondArray.includes(item));
 }
