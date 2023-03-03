dcd5229d1765a2af153584abba9c005bb0671e38
Wederson Calixto <wederson.programador@gmail.com>
2023-03-03 19:50:11 -0300

    reafactor: all implementations

--- big-O/exercises/find-nemo.ts
+++ big-O/exercises/find-nemo.ts
@@ -1,33 +1,31 @@
 // deno-lint-ignore-file no-unused-vars
 
-// Theses functions will loop through an array of names and show the message "Found DENO!"
-// for every name that is equal to Nemo
+// These functions will loop through an array of names and show the message
+// "Found DENO!" for every name that is equal to Nemo
 
 // O(n) --> Linear Time
 function findNemo(fishNames: string[]): void {
   for (let index = 0; index < fishNames.length; index++) {
-    if (isNemo(fishNames[index])) foundNemo();
+    checkIfThatIsNemo(fishNames.at(index)!);
   }
 }
 
 // O(n) --> Linear Time
 function findNemo2(fishNames: string[]): void {
-  fishNames.forEach((fish: string) => {
-    if (isNemo(fish)) foundNemo();
+  fishNames.forEach((name: string) => {
+    checkIfThatIsNemo(name);
   });
 }
 
 // O(n) --> Linear Time
 function findNemo3(fishNames: string[]): void {
-  for (const fish of fishNames) {
-    if (isNemo(fish)) foundNemo();
+  for (const name of fishNames) {
+    checkIfThatIsNemo(name);
   }
 }
 
-function isNemo(name: string): boolean {
-  return name === "nemo";
-}
-
-function foundNemo(): void {
-  console.log("Found NEMO!");
+function checkIfThatIsNemo(fishName: string): void {
+  if (fishName.toLowerCase() === "nemo") {
+    console.log("Found NEMO!");
+  }
 }
